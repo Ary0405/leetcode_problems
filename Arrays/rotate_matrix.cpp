05894220ef23325ffe78e37d2100fc44ea04ecfd
Ary0405 <aryansethia2003@gmail.com>
2024-06-03 20:34:34 +0530

    problem 48 added

---
+++ Arrays/rotate_matrix.cpp
@@ -0,0 +1,28 @@
+// Problem - 48 (Rotate Image)
+// Time Complexity - O(n^2)
+// Space Complexity - O(1)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+
+    for (int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < i ; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        reverse(matrix.begin(), matrix.end());
+    }
+
+    return;
+}

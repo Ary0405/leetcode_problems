f7f2d1336fee8815522570d8cdc5ef80b66e25c3
Ary0405 <aryansethia2003@gmail.com>
2024-06-03 09:41:52 +0530

    problem 53 added

---
+++ Arrays/kadane_algorithm.cpp
@@ -0,0 +1,23 @@
+// Problem - 53 (Maximum Subarray)
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int maxSubArray(vector<int> &nums)
+{
+    int maxSum = INT_MIN;
+    int n = nums.size();
+    int currSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        currSum += nums[i];
+        maxSum = max(maxSum, currSum);
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
+    }
+    return maxSum;
+}

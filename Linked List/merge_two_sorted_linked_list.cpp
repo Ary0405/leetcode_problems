55fd5505b10ceb0d5acda9b9edfcfef57ae449b4
Ary0405 <aryansethia2003@gmail.com>
2024-06-06 21:36:56 +0530

    problem 21 added

---
+++ Linked List/merge_two_sorted_linked_list.cpp
@@ -0,0 +1,71 @@
+// Problem - 21 (Merge Two Sorted Lists)
+// Time Complexity = O(n)
+// Space Complexity = O (1)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+{
+    if (list1 == NULL)
+    {
+        return list2;
+    }
+    if (list2 == NULL)
+    {
+        return list1;
+    }
+
+    ListNode *newHead = NULL;
+    ListNode *current = NULL;
+
+    if (list1->val < list2->val)
+    {
+        newHead = list1;
+        list1 = list1->next;
+    }
+    else
+    {
+        newHead = list2;
+        list2 = list2->next;
+    }
+
+    current = newHead;
+
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->val < list2->val)
+        {
+            current->next = list1;
+            list1 = list1->next;
+        }
+        else
+        {
+            current->next = list2;
+            list2 = list2->next;
+        }
+        current = current->next;
+    }
+
+    if (list1 != NULL)
+    {
+        current->next = list1;
+    }
+    else
+    {
+        current->next = list2;
+    }
+
+    return newHead;
+}

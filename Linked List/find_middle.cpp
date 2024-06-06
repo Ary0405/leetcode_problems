9b9b17e67b7a6a2822f8195f200296a3766270cb
Ary0405 <aryansethia2003@gmail.com>
2024-06-06 21:03:07 +0530

    problem 876 added

---
+++ Linked List/find_middle.cpp
@@ -0,0 +1,41 @@
+// Problem - 876 (Middle of the Linked List)
+// Time Complexity = O(n)
+// Space Complexity = O (1)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *middleNode(ListNode *head)
+{
+    if (head == NULL)
+    {
+        return head;
+    }
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main()
+{
+    // can be completed
+}

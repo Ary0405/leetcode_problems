2f39b1914b26f7bfa3bf4a46f8a2d31560b1faec
Ary0405 <aryansethia2003@gmail.com>
2024-06-07 13:29:23 +0530

    problem 141 added

---
+++ Linked List/linked_list_cycle.cpp
@@ -0,0 +1,44 @@
+// Problem - 141 (Linked List Cycle)
+// Time Complexity = O(n)
+// Space Complexity = O (1)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+bool hasCycle(ListNode *head)
+{
+    if (head == NULL)
+    {
+        return false;
+    }
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    // can be completed
+}

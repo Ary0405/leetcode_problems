86dbc83e9b8ba845963a1624290c2a160a08675f
Ary0405 <aryansethia2003@gmail.com>
2024-06-20 13:31:16 +0530

    yo

---
+++ String/reverse_words.cpp
@@ -0,0 +1,50 @@
+// Problem - 3 (Longest Substring Without Repeating Characters)
+// Time Complexity = O(n)
+// Space Complexity = O (1)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string reverseWords(string s)
+{
+    vector<string> words;
+    int i = 0;
+    while (i < s.length())
+    {
+        if (s[i] == ' ')
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+            string temp;
+            while (i < s.length() && s[i] != ' ')
+            {
+                temp += s[i];
+                i++;
+            }
+            words.push_back(temp);
+        }
+    }
+    string ans;
+    for (int j = words.size() - 1; j >= 0; j--)
+    {
+        ans += words[j];
+        if (j != 0)
+        {
+            ans += " ";
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    // can be completed
+    string s = "hi i am good";
+    cout << reverseWords(s) << endl;
+    return 0;
+}

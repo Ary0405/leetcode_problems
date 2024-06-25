4f95520ed139252825cce96be93bc1f38e291bec
Ary0405 <aryansethia2003@gmail.com>
2024-06-25 21:57:39 +0530

    top view of a binary tree added

---
+++ Binary Tree/top_view.cpp
@@ -0,0 +1,82 @@
+// Problem - Top View of a Binary Tree (Geeks for Geeks)
+// Time Complexity - O(nlog(n))
+// Space Complexity - O(n)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+vector<int> topView(Node *root)
+{
+    // Your code here
+    vector<int> ans;
+    if (root == NULL)
+    {
+        return ans;
+    }
+
+    map<int, Node *> mp;
+
+    queue<pair<Node *, int>> q;
+    q.push({root, 0});
+
+    mp[0] = root;
+
+    while (!q.empty())
+    {
+        int n = q.size();
+        for (int i = 1; i <= n; i++)
+        {
+            Node *f = q.front().first;
+            int l = q.front().second;
+            q.pop();
+            bool isThere = false;
+            if (mp.find(l) != mp.end())
+            {
+                isThere = true;
+            }
+
+            if (isThere == false)
+            {
+                mp[l] = f;
+            }
+
+            if (f->left)
+            {
+                q.push({f->left, l - 1});
+            }
+            if (f->right)
+            {
+                q.push({f->right, l + 1});
+            }
+        }
+    }
+
+    map<int, Node *>::iterator it;
+    for (it = mp.begin(); it != mp.end(); ++it)
+    {
+        ans.push_back(it->second->data);
+    }
+    return ans;
+}
+
+int main()
+{
+    // can be completed
+}
